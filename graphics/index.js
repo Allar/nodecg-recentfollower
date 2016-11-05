1e23af093d988643825f8abfb57d807e5632b1dd
Michael Allar <allar@michaelallar.com>
2016-11-04 18:12:49 -0700

    Added platform icons

--- graphics/index.js
+++ graphics/index.js
@@ -1,16 +1,25 @@
 'use strict';
-
-
 var RecentFollower = nodecg.Replicant('RecentFollower');
+var RecentFollowerPlatform = nodecg.Replicant('RecentFollowerPlatform', {defaultValue: 'beam'});
+
+function setClassVisibility(classname, visible){
+    var elements = document.getElementsByClassName(classname);
+
+    for (var i = 0; i < elements.length; i++){
+        elements[i].style.display = visible ? 'inline' : 'none';
+    }
+}
 
 nodecg.listenFor('channel-followed', 'nodecg-beam-service', function(user) {
     RecentFollower.value = user.username;
+    RecentFollowerPlatform.value = 'beam';
+    
 });
 
 nodecg.listenFor('channel-followed', 'nodecg-twitch-service', function(user) {
     RecentFollower.value = user.display_name;
+    RecentFollowerPlatform.value = 'twitch';
 });
-
 
 var alertBox = document.getElementById('alert');
 alertBox.addEventListener('webkitAnimationEnd', function(){
@@ -19,6 +28,11 @@ alertBox.addEventListener('webkitAnimationEnd', function(){
 
 RecentFollower.on('change', function(newvalue, oldvalue) {
     alertBox.classList.remove('recentFollowerAnimDownClass');
-    document.getElementById("RecentFollower").firstElementChild.textContent = newvalue;
+    document.getElementById("RecentFollowerText").textContent = newvalue;
     setTimeout( () => { alertBox.classList.add('recentFollowerAnimDownClass') }, 50);
+});
+
+RecentFollowerPlatform.on('change', (newvalue, oldvalue) => {
+    setClassVisibility('beam', newvalue == 'beam');
+    setClassVisibility('twitch', newvalue == 'twitch');
 });
